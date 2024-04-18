a40975c36294e4988275bdda56f89761c1504c38
Prajwal <prajwalgnaik12@gmail.com>
2024-04-18 15:51:32 +0530

    model_rem

--- app1.py
+++ app1.py
@@ -1,11 +1,13 @@
 # app.py
-from flask import Flask, request, jsonify,send_file
+from flask import Flask, request
 from flask_cors import CORS
 import subprocess
 import os
+import json
 
 app = Flask(__name__)
 CORS(app)
+# CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 
 @app.route('/send_input', methods=['POST'])
@@ -15,14 +17,29 @@ def receive_input():
         received_input = data['input']
         print("Received input:", received_input)
         binary_path='/home/prajwal/Desktop/project/webscraper'
-        result=subprocess.check_output([binary_path,received_input, str(50)],stderr=subprocess.STDOUT)
-        # output_file_path = '/home/prajwal/Desktop/project/output.json'
+        result=subprocess.check_output([binary_path,received_input, str(30)],stderr=subprocess.STDOUT)
+        output_file_path = '/home/prajwal/Desktop/project/output.json'
+
+        summary_file_path = '/home/prajwal/Desktop/project/summary.json'
+
+        with open(output_file_path, 'r') as f1:
+            data1 = json.load(f1)
+            
+        with open(summary_file_path, 'r') as f2:
+            data2 = json.load(f2)
+        print(data1)
+            
+            # Create a dictionary containing both data
+        response_data = {
+            "review": data1,
+            "summary": data2
+        }
         
+        return response_data
         # if os.path.exists(output_file_path):
-        #     return send_file(output_file_path, as_attachment=True)
+        #     return 
         # else:
-        #     return result
-        return result
+        #     return "response"
     else:
         return 'Invalid input data', 400
 
--- user/src/components/ReviewPage/ReviewPage.jsx
+++ user/src/components/ReviewPage/ReviewPage.jsx
@@ -1,66 +1,30 @@
-// // ReviewPage.jsx
-// import React, { useEffect, useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-
-// const ReviewPage = () => {
-//   const [reviews, setReviews] = useState([]);
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     const searchParams = new URLSearchParams(location.search);
-//     const productUrl = searchParams.get('url');
-
-//     // Mocking fetching reviews from the provided URL
-//     fetchReviews(productUrl);
-//   }, [location.search]);
-
-//   const fetchReviews = (productUrl) => {
-//     // Here you would typically make an API call to fetch reviews
-//     // For this example, I'm just mocking the data
-//     const mockReviews = [
-//       'Design: Sleek and stylish design with premium materials.',
-//       'Display: Vibrant and sharp OLED display with True Tone technology.',
-//       'Performance: Blazing fast performance thanks to the latest A-series chip.',
-//       'Camera: Excellent camera quality with impressive low-light performance and advanced photography features.',
-//       'Battery Life: Improved battery life compared to previous models, but may still require daily charging with heavy usage.',
-//       'Software: Smooth and intuitive user experience with regular updates and a wide range of apps available on the App Store.',
-//       'Security: Enhanced security features such as Face ID for biometric authentication and robust privacy settings.',
-//       'Connectivity: Fast 5G connectivity for faster download and streaming speeds (on compatible models).',
-//       'Price: Premium pricing compared to other smartphones on the market, but offers great value for its features and performance.',
-//       'Overall: The iPhone delivers a premium smartphone experience with top-notch build quality, performance, and features, making it a popular choice among users worldwide.'
-//     ];
-//     setReviews(mockReviews);
-//   };
-
-//   return (
-//     <div classNameName='review-container'>
-//       <h1>Product Reviews</h1>
-//       <ul classNameName='reviews'>
-//         {reviews.map((review, index) => (
-//           <li key={index}>{review}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ReviewPage;
-
-// ReviewPage.jsx
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import testReviews from "../reviews.json";
 import "./ReviewPage.css";
 import Rating from '@mui/material/Rating';
 import axios from 'axios'
 import { uniqueNamesGenerator, Config, names } from 'unique-names-generator';
 
+// console.log(testSummary["positive","neg"])
+// console.log(testSummary["positive", "neg", "1-star", "2-star", "3-star", "4-star", "5-star"]);
+
 
 const ReviewPage = () => {
-  const [reviews, setReviews] = useState([]);
+  const [reviews, setReviews] = useState({
+    rating: '',
+    review: '',
+  });
   const location = useLocation();
   const [data, setData] = useState()
 
+
+  // const ReviewPage = () => {
+  //   const [summary, setSummary] = useState({
+  //     rating:'',
+  //     review:'',
+  //   });
+  //   const location = useLocation();
+  //   const [data, setData] = useState([])
 
 
   useEffect(() => {
@@ -79,38 +43,42 @@ const ReviewPage = () => {
       axios.post("http://localhost:5000/send_input", { input: productUrl }).then(res => {
         console.log(res.data)
         setData(res.data)
+        // console.log("Hii"+JSON.stringify(data[0]).review)
       })
+      // fetch("http://localhost:5000/send_input",
+      //   {
+      //     headers: {
+      //       'Accept': 'application/json',
+      //       'Content-Type': 'application/json'
+      //     },
+      //     method: "POST",
+      //     body: JSON.stringify({ input: productUrl })
+      //   })
+      //   .then(function (res) { console.log(res) })
+      //   .catch(function (res) { console.log(res) })
     } catch (error) {
       console.error("Error fetching reviews:", error);
     }
   };
 
 
+
   const config = {
     dictionaries: [names]
   }
-  
-  
+
+
   return (
-    // <div classNameName='review-container'>
-    //   <h1>Product Reviews</h1>
-    //   <ul classNameName='reviews'>
-    //     {testReviews.data.map((review, index) => (
-    //       <li key={index}>
-    //         Rating: {review.rating}, Review: {review.review}
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
     <div className="flex flex-row gap-5">
       <section className="testimonials">
+        {/* <div className="testimonials-item1"> */}
         <div className="section-header">
           <h2 className="title">Product Reviews</h2>
         </div>
         <div className="testimonials-content">
           <div className="testimonials-slider js-testimonials-slider">
             <div>
-              {testReviews.data.map((review, index) => {
+              {data?.review.data.map((review, index) => {
                 return (
                   <div className="testimonials-item">
                     <div className="info">
@@ -129,13 +97,75 @@ const ReviewPage = () => {
                 );
               })}
             </div>
+          </div>
+        </div>
+        {/* </div> */}
+      </section>
+      <div className="testimonials-item w-4/12" >
+        <div className="section-header">
+          <h2 className="title">Product Summary</h2>
+        </div>
+
+        <div className="testimonials-item1">
+          <div >Positive Review</div>
+          <div className="testimonials-item">
+            {/* {testSummary["positive"]} */}
+            {data?.summary.key1}
+          </div>
+        </div>
+        <div className="testimonials-item">
+          <div >Negetive Review</div>
+          <div className="testimonials-item">
+            {data?.summary.key2}
+
+            {/* {testSummary["negetive"]} */}
+          </div>
+        </div>
+
+        <div className="testimonials-item">
+          <div>⭐ Review Summary</div>
+          <div className="testimonials-item">
+            {/* {testSummary["1-star"]} */}
+            {data?.summary.key1}
 
           </div>
         </div>
 
-      </section>
-      <div className="testimonials-item w-4/12" >
-        {data}
+        <div className="testimonials-item">
+          <div >⭐⭐ Review Summary</div>
+          <div className="testimonials-item">
+            {/* {testSummary["2-star"]} */}
+            {data?.summary.key1}
+
+          </div>
+        </div>
+
+        <div className="testimonials-item">
+          <div >⭐⭐⭐ Review Summary</div>
+          <div className="testimonials-item">
+            {/* {testSummary["3-star"]} */}
+            {data?.summary.key1}
+
+          </div>
+        </div>
+
+        <div className="testimonials-item">
+          <div >⭐⭐⭐⭐ Review Summary</div>
+          <div className="testimonials-item">
+            {/* {testSummary["4-star"]} */}
+            {data?.summary.key1}
+
+          </div>
+        </div>
+
+        <div className="testimonials-item">
+          <div >⭐⭐⭐⭐⭐ Review Summary</div>
+          <div className="testimonials-item">
+            {/* {testSummary["5-star"]} */}
+            {data?.summary.key1}
+
+          </div>
+        </div>
       </div>
     </div>
 
